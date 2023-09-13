7b4db73d2db6c69a6ca23ae235bd6a26b0bbe574
nitzanto <nitzan16001@gmail.com>
2023-09-13 15:19:52 +0300

    * Added controllers and endpoints for the API

---
+++ src/ninjas/ninjas.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+
+@Controller('ninjas')
+export class NinjasController {
+  @Get()
+  getNinjas() {
+    return [];
+  }
+
+  @Get(':id')
+  getOneNinja() {
+    return {};
+  }
+
+  @Post()
+  createNinja() {
+    return [];
+  }
+
+  @Put(':id')
+  updateNinja() {
+    return {};
+  }
+
+  @Delete(':id')
+  removeNinja() {
+    return {};
+  }
+}
